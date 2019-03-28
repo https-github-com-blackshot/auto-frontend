3dd090a31919e36f378c5aad1d04c6a821b65f01
Suleimenov Adilbek <suleiimenov@gmail.com>
2019-03-28 23:09:13 +0600

    roles model

---
+++ src/app/models/roles.ts
@@ -0,0 +1,11 @@
+export class Roles {
+    constructor(
+        public id?: number,
+        public code?: string,
+        public nameRu?: string,
+        public nameKZ?: string,
+        public nameEn?: string
+    ) {}
+
+
+}
