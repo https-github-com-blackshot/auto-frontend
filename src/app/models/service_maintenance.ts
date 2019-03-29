1a7bcb7b22826c9e4f43f899e68b0a170ad7011d
Alikhan Makhmed <makhmedov.12@mail.ru>
2019-03-29 10:38:31 +0600

    my models

---
+++ src/app/models/service_maintenance.ts
@@ -0,0 +1,12 @@
+export class ServiceMaintenance {
+
+    constructor(
+        public id?: number,
+        public name?: string,
+        public description?: string,
+        public address?: string,
+        public phone_number?: string,
+        public rating_id?: number) {}
+
+
+}
