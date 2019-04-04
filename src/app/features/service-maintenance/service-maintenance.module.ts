c30245596deaaf4511c5c99604168a90a07e944a
Suleimenov Adilbek <suleiimenov@gmail.com>
2019-04-04 22:22:00 +0600

    Service Maintenance components

--- src/app/features/service-maintenance/service-maintenance.module.ts
+++ src/app/features/service-maintenance/service-maintenance.module.ts
@@ -6,13 +6,16 @@ import {HttpClientModule} from '@angular/common/http';
 import { ServiceMaintenanceComponent } from './service-maintenance.component';
 import {ServiceMaintenanceService} from './service-maintenance.service';
 import {ServiceMaintenanceRoutingModule} from './service-maintenance-routing.module';
+import {MatButtonModule, MatCardModule} from '@angular/material';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     HttpClientModule,
-    ServiceMaintenanceRoutingModule
+    ServiceMaintenanceRoutingModule,
+    MatButtonModule,
+    MatCardModule
   ],
   declarations: [ServiceMaintenanceComponent],
   providers: [ServiceMaintenanceService],
