c410f4a6bae515d91b20db2bb4898bb56507e0dc
tima-kh-97 <tima.kh.97@gmail.com>
2019-05-03 11:30:49 +0600

    rating

--- src/app/features/service-maintenance/components/service-maintenance-detail/service-maintenance-detail.component.ts
+++ src/app/features/service-maintenance/components/service-maintenance-detail/service-maintenance-detail.component.ts
@@ -55,8 +55,69 @@ export class ServiceMaintenanceDetailComponent implements OnInit, OnDestroy {
         this._serviceMaintenanceService.getRating(this.serviceMaintenance.ratingId)
             .pipe(takeUntil(this._unsubscribeAll)).subscribe((res => {
             this.rating = res;
-            this.overallRating = this.rating.veryLow + this.rating.low + this.rating.medium + this.rating.high + this.rating.veryHigh;
-        }))
+            console.log('rating', this.rating);
+            this.overallRating = (this.rating.veryLow + this.rating.low + this.rating.medium + this.rating.high + this.rating.veryHigh) / 2;
+                console.log('overallRating', this.overallRating);
+            }));
+    }
+
+    giveRating(score: number): void {
+        if (this.rating !== undefined && this.rating !== null) {
+            switch (score) {
+                case 1:
+                    this.rating.veryLow++;
+                    break;
+                case 2:
+                    this.rating.low++;
+                    break;
+                case 3:
+                    this.rating.medium++;
+                    break;
+                case 4:
+                    this.rating.high++;
+                    break;
+                case 5:
+                    this.rating.veryHigh++;
+                    break;
+            }
+            this._serviceMaintenanceService.updateRating(this.rating)
+                .pipe(takeUntil(this._unsubscribeAll)).subscribe((res) => {
+                    this.rating = res;
+                this.serviceMaintenance.ratingId = this.rating.id;
+                this._serviceMaintenanceService.updateServiceMaintenance(this.serviceMaintenance)
+                    .pipe(takeUntil(this._unsubscribeAll)).subscribe((response) => {
+                    this.serviceMaintenance = response;
+                });
+            })
+        } else {
+            this.rating = new Rating();
+            switch (score) {
+                case 1:
+                    this.rating.veryLow = 1;
+                    break;
+                case 2:
+                    this.rating.low = 1;
+                    break;
+                case 3:
+                    this.rating.medium = 1;
+                    break;
+                case 4:
+                    this.rating.high = 1;
+                    break;
+                case 5:
+                    this.rating.veryHigh = 1;
+                    break;
+            }
+            this._serviceMaintenanceService.createRating(this.rating)
+                .pipe(takeUntil(this._unsubscribeAll)).subscribe((res) => {
+                this.rating = res;
+                this.serviceMaintenance.ratingId = this.rating.id;
+                this._serviceMaintenanceService.updateServiceMaintenance(this.serviceMaintenance)
+                    .pipe(takeUntil(this._unsubscribeAll)).subscribe((response) => {
+                     this.serviceMaintenance = response;
+                });
+            });
+        }
     }
 
 }
